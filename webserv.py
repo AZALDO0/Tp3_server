5445d754e8f9e95d82b3321bda95e70faa89afaa
Malik BADAROU <mbadarou@et.esiea.fr>
2022-03-31 14:50:26 +0200

    Mise à jour tp3_server

---
+++ webserv.py
@@ -0,0 +1,31 @@
+from imp import reload 
+from typing import Optional
+from fastapi import FastAPI
+from shodan import Shodan
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+
+
+@app.get("/ip/{ip}")
+async def get_ip(ip: str, key: Optional[str] = None):
+    if key is None:
+        return {"Error": "Please provide a valid API key"}
+    else:
+        try:
+            api = Shodan(key)
+            res = api.host(ip)
+            return {
+               # "IP": res["ip_str"],
+               # "Organization": res["org"],
+                #"Country": res["country_name"],
+                "lat": res["latitude"],
+                "long": res["longitude"]
+            }
+        except Exception as e:
+            return {"Error": str(e)}
